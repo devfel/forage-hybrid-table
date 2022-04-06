50a72bfede48d3e024defcfb423038a5c8041328
devfel <devfelizardo@gmail.com>
2022-04-06 01:52:21 -0400

    changing the footer of chart page

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -546,7 +546,7 @@ export function TransactionTable() {
         </ScatterChart>
 
         <div className="footer">
-          <span>*All the information were gathered from the University of Florida website.</span>
+          <span>*All the information were gathered from the University of Florida Corn Silage and Forage Field Day WebSite.</span>
           <span>
             Visit{" "}
             <a href="https://animal.ifas.ufl.edu/extension/courses/csfd/" target="_blank" rel="noopener noreferrer">
@@ -555,6 +555,7 @@ export function TransactionTable() {
             </a>{" "}
             for more details.
           </span>
+          <span>Contact us at <a href="mailto: forages@ifas.ufl.edu">forages@ifas.ufl.edu</a>.</span>
         </div>
       </Container>
     );
