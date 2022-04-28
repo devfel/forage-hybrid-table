a6567ebecb0c346f004ecfa1e5bd8b49bfc42fbb
devfel <devfelizardo@gmail.com>
2022-04-28 18:48:11 -0400

    changing the system footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,9 @@ export function Footer() {
     <Container>
       <Content>
         <div className="footer">
-          <span>*All the information were gathered from the University of Florida Corn Silage and Forage Field Day WebSite.</span>
+          <span style={{ color: "darkblue", fontWeight: "bold" }}>Blue and bold characteristics in the table mean it was a top performer within that season and year.</span>
+          <br></br>
+          <span>All the information were gathered from the University of Florida Corn Silage and Forage Field Day WebSite.</span>
           <span>
             Visit{" "}
             <a href="https://animal.ifas.ufl.edu/extension/courses/csfd/" target="_blank" rel="noopener noreferrer">
